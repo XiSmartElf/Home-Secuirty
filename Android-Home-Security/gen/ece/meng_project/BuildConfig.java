df806fc7dd58676c8b72d35fe3b95688551056be
Xi He <xh243@cornell.edu>
2015-06-30 04:11:11 -0400

    init3

---
+++ Android-Home-Security/gen/ece/meng_project/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ece.meng_project;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- DemoActivity/gen/ece/meng_project/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ece.meng_project;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
